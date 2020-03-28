14b8e1b0e1558c92f93607bffcbaa0cdf1dae713
Damilola Oluwafemi <damilolaoluwafemi@Damilolas-MacBook-Pro.local>
2020-03-28 04:31:09 +0100

    chore: open create post modal from tabs page

--- src/app/tabs/tabs.module.ts
+++ src/app/tabs/tabs.module.ts
@@ -1,18 +1,21 @@
-import { IonicModule } from '@ionic/angular';
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { IonicModule } from "@ionic/angular";
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { FormsModule } from "@angular/forms";
 
-import { TabsPageRoutingModule } from './tabs-routing.module';
+import { TabsPageRoutingModule } from "./tabs-routing.module";
 
-import { TabsPage } from './tabs.page';
+import { TabsPage } from "./tabs.page";
+import { CreatePostPageModule } from "../create-post/create-post.module";
 
 @NgModule({
   imports: [
     IonicModule,
     CommonModule,
     FormsModule,
-    TabsPageRoutingModule
+    IonicModule,
+    TabsPageRoutingModule,
+    CreatePostPageModule
   ],
   declarations: [TabsPage]
 })
--- src/app/tabs/tabs.page.ts
+++ src/app/tabs/tabs.page.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { ModalController, IonRouterOutlet } from "@ionic/angular";
+import { CreatePostPage } from "../create-post/create-post.page";
 
 @Component({
-  selector: 'app-tabs',
-  templateUrl: 'tabs.page.html',
-  styleUrls: ['tabs.page.scss']
+  selector: "app-tabs",
+  templateUrl: "tabs.page.html",
+  styleUrls: ["tabs.page.scss"]
 })
 export class TabsPage {
+  constructor(
+    private modalController: ModalController,
+    private routerOutlet: IonRouterOutlet
+  ) {}
 
-  constructor() {}
-
+  async openCreatePostModal() {
+    const modal = await this.modalController.create({
+      component: CreatePostPage,
+      swipeToClose: true,
+      presentingElement: this.routerOutlet.nativeEl
+    });
+    return await modal.present();
+  }
 }
