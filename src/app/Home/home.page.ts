d11544df662f0eca94021225797c680bd4207a83
Damilola Oluwafemi <damilolaoluwafemi@Damilolas-MacBook-Pro.local>
2020-04-08 18:08:50 +0100

    chore: fetch  live data for feed and user profile

--- src/app/Home/home.page.ts
+++ src/app/Home/home.page.ts
@@ -1,22 +1,45 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 import { HomeService } from "./home.service";
 import { Post } from "../models/post.model";
+import { Storage } from "@ionic/storage";
+import { Profile } from "../models/profile.model";
 
 @Component({
   selector: "app-home",
   templateUrl: "home.page.html",
-  styleUrls: ["home.page.scss"]
+  styleUrls: ["home.page.scss"],
 })
-export class HomePage {
+export class HomePage implements OnInit {
   posts: Post[];
+  profileId: string = "";
+  profileDetails: Profile;
 
-  constructor(private homeService: HomeService) {
+  constructor(private homeService: HomeService, private storage: Storage) {
     this.homeService.getAllPosts();
+    this.initGetUserDetails();
+    this.subscribeTogetUserDetails();
   }
 
   ionViewWillEnter() {
-    this.homeService.allPosts$.subscribe(res => {
+    this.homeService.allPosts.subscribe((res) => {
       this.posts = res;
+    });
+  }
+
+  ngOnInit() {}
+
+  initGetUserDetails() {
+    this.storage.get("profileId").then((res) => {
+      if (res) {
+        this.profileId = res;
+        this.homeService.getUserById(this.profileId);
+      }
+    });
+  }
+
+  subscribeTogetUserDetails() {
+    this.homeService.profileDetails.subscribe((res) => {
+      this.profileDetails = res;
     });
   }
 }
--- src/app/Home/home.service.ts
+++ src/app/Home/home.service.ts
@@ -1,61 +1,62 @@
-import { Injectable } from "@angular/core";
+import { Injectable, OnDestroy } from "@angular/core";
 import { Post } from "../models/post.model";
-import { BehaviorSubject } from "rxjs";
+import { BehaviorSubject, Observable } from "rxjs";
+import { HttpClient } from "@angular/common/http";
+import { environment } from "src/environments/environment";
+import { retry, takeWhile } from "rxjs/operators";
+import { Profile } from "../models/profile.model";
 
 @Injectable({
-  providedIn: "root"
+  providedIn: "root",
 })
-export class HomeService {
-  private allPosts = new BehaviorSubject<Post[]>(null);
-  allPosts$ = this.allPosts.asObservable();
+export class HomeService implements OnDestroy {
+  allPosts = new BehaviorSubject(null);
+  profileDetails = new BehaviorSubject<Profile>(null);
+  posts: Post[] = [];
 
-  constructor() {}
+  alive: boolean;
 
-  posts: Post[] = [
-    {
-      id: "r1",
-      title: "Heineken",
-      imageUrl:
-        "https://images.pexels.com/photos/70598/pexels-photo-70598.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260",
-      description:
-        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veritatis accusamus ea ullam, ducimus inventore iure maxime cumque error repellendus odio expedita deserunt fuga optio sapiente, soluta, est tempora quasi praesentium Deserunt repellat voluptas totam atque fugiat. Tenetur repellat corporis possimus quo perspiciatis, iste sunt non et repellendus qui, animi maxime facilis quae cum numquam delectus aut autem incidunt a",
-      tags: ["cruise", "smooth"]
-    },
-    {
-      id: "r2",
-      title: "Budweiser",
-      imageUrl:
-        "https://images.pexels.com/photos/573910/pexels-photo-573910.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
-      description: "The best beer in the world",
-      tags: ["cruise", "smooth"]
-    },
-    {
-      id: "r3",
-      title: "Corona Extra",
-      imageUrl:
-        "https://images.pexels.com/photos/1089932/pexels-photo-1089932.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
-      description: "The best beer in the world",
-      tags: ["cruise", "smooth"]
-    },
-    {
-      id: "r4",
-      title: "Veza",
-      imageUrl:
-        "https://images.pexels.com/photos/1267696/pexels-photo-1267696.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
-      description: "Me and the gang enjoying the beer",
-      tags: ["cruise", "smooth"]
-    }
-  ];
+  constructor(private http: HttpClient) {
+    this.alive = true;
+  }
+
+  ngOnDestroy() {
+    this.alive = false;
+  }
+
+  _getUserById(id: string): Observable<any> {
+    return this.http.get(`${environment.registerUser}/${id}`).pipe(retry(2));
+  }
+
+  getUserById(id: string) {
+    this._getUserById(id)
+      .pipe(takeWhile(() => this.alive))
+      .subscribe((res) => {
+        this.profileDetails.next(res);
+      });
+  }
+
+  _getAllPosts(): Observable<any> {
+    return this.http.get(environment.getFeed);
+  }
 
   getAllPosts() {
-    this.allPosts.next(this.posts);
+    this._getAllPosts().subscribe((res) => {
+      if (res) {
+        this.allPosts.next(res);
+        this.posts = res;
+      } else {
+        console.log("unable to retrieve posts");
+      }
+    });
   }
 
   getPostById(postId: string) {
+    console.log(postId, "postID::::");
     return {
-      ...this.posts.find(post => {
-        return post.id === postId;
-      })
+      ...this.posts.filter((post) => {
+        return post.id == postId;
+      }),
     };
   }
 
@@ -64,7 +65,7 @@ export class HomeService {
   }
 
   deleteRecipe(postId: string) {
-    this.posts = this.posts.filter(post => {
+    this.posts = this.posts.filter((post) => {
       return post.id !== postId;
     });
   }
