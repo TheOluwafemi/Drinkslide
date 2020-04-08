bce2233debdfed823926c73ee09fa9808f98adaa
Damilola Oluwafemi <damilolaoluwafemi@Damilolas-MacBook-Pro.local>
2020-04-08 18:07:02 +0100

    feat: added user profile picture to header

--- src/app/components/header-scroll/header-scroll.component.ts
+++ src/app/components/header-scroll/header-scroll.component.ts
@@ -1,15 +1,32 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, Input } from "@angular/core";
 import { MenuController } from "@ionic/angular";
+import { HomeService } from "src/app/Home/home.service";
+import { Profile } from "src/app/models/profile.model";
 
 @Component({
   selector: "app-header-scroll",
   templateUrl: "./header-scroll.component.html",
-  styleUrls: ["./header-scroll.component.scss"]
+  styleUrls: ["./header-scroll.component.scss"],
 })
 export class HeaderScrollComponent implements OnInit {
-  constructor(private menuController: MenuController) {}
+  profilePicture: string;
 
-  ngOnInit() {}
+  constructor(
+    private menuController: MenuController,
+    private homeService: HomeService
+  ) {}
+
+  ngOnInit() {
+    this.getProfilePicture();
+  }
+
+  getProfilePicture() {
+    this.homeService.profileDetails.subscribe((res) => {
+      if (res) {
+        this.profilePicture = res["profile_picture"];
+      }
+    });
+  }
 
   async openMenu() {
     await this.menuController.open();
