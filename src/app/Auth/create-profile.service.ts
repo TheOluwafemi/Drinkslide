903622a9c6be2cc80e939b3db9b78d87c636af7f
Damilola Oluwafemi <damilolaoluwafemi@Damilolas-MacBook-Pro.local>
2020-04-08 09:37:25 +0100

    feat: create new account

---
+++ src/app/Auth/create-profile.service.ts
@@ -0,0 +1,45 @@
+import { Injectable, OnDestroy } from "@angular/core";
+import { Observable, BehaviorSubject } from "rxjs";
+import { environment } from "src/environments/environment";
+import { HttpClient, HttpErrorResponse } from "@angular/common/http";
+import { retry, catchError, takeWhile } from "rxjs/operators";
+
+@Injectable({
+  providedIn: "root",
+})
+export class CreateProfileService implements OnDestroy {
+  alive: boolean;
+  registerState = new BehaviorSubject(null);
+
+  constructor(private http: HttpClient) {}
+
+  _register(
+    email: string,
+    username: string,
+    password: string
+  ): Observable<any> {
+    return this.http
+      .post(environment.registerUser, {
+        email: email,
+        name: username,
+        password: password,
+      })
+      .pipe(retry(2));
+  }
+
+  register(email, username, password) {
+    this._register(email, username, password)
+      .pipe(takeWhile(() => this.alive))
+      .subscribe((res) => {
+        if (res) {
+          this.registerState.next(res);
+        } else {
+          console.log("Unable to register user");
+        }
+      });
+  }
+
+  ngOnDestroy() {
+    this.alive = false;
+  }
+}
--- src/app/register/register.page.ts
+++ src/app/register/register.page.ts
@@ -1,15 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { CreateProfileService } from "../Auth/create-profile.service";
+import { Router } from "@angular/router";
 
 @Component({
-  selector: 'app-register',
-  templateUrl: './register.page.html',
-  styleUrls: ['./register.page.scss'],
+  selector: "app-register",
+  templateUrl: "./register.page.html",
+  styleUrls: ["./register.page.scss"],
 })
 export class RegisterPage implements OnInit {
+  email: string;
+  username: string;
+  password: string;
 
-  constructor() { }
+  constructor(
+    private createProfileService: CreateProfileService,
+    private router: Router
+  ) {}
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  onRegister() {
+    this.createProfileService.register(
+      this.email,
+      this.username,
+      this.password
+    );
+    this.createProfileService.registerState.subscribe((res) => {
+      console.log(res);
+      if (res) {
+        this.router.navigateByUrl("/login");
+      } else {
+        console.log("unable to register user");
+      }
+    });
   }
-
 }
