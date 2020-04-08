a1d75adf261f6a52ee1f916a7861275ce79eb5cb
Damilola Oluwafemi <damilolaoluwafemi@Damilolas-MacBook-Pro.local>
2020-04-08 09:36:52 +0100

    feat: login, checktoken, logout, store token

---
+++ src/app/Auth/authentication.service.ts
@@ -0,0 +1,104 @@
+import { Injectable, OnDestroy } from "@angular/core";
+import { Storage } from "@ionic/storage";
+import { Platform, AlertController } from "@ionic/angular";
+import {
+  HttpClient,
+  HttpResponse,
+  HttpErrorResponse,
+} from "@angular/common/http";
+import { environment } from "src/environments/environment";
+import { retry, catchError, shareReplay, takeWhile } from "rxjs/operators";
+import { throwError, Observable, BehaviorSubject } from "rxjs";
+import { Router } from "@angular/router";
+
+@Injectable({
+  providedIn: "root",
+})
+export class AuthenticationService implements OnDestroy {
+  token: string;
+  alive: boolean;
+  loginState = new BehaviorSubject(null);
+  authenticationState = new BehaviorSubject(false);
+
+  constructor(
+    private storage: Storage,
+    private platform: Platform,
+    private http: HttpClient,
+    private alertController: AlertController,
+    private router: Router
+  ) {
+    this.alive = true;
+    this.platform.ready().then(() => {
+      this.checkToken();
+    });
+  }
+
+  checkToken() {
+    this.storage.get("token").then((res) => {
+      if (res) {
+        this.token = res;
+        this.authenticationState.next(true);
+      }
+    });
+  }
+
+  setToken(token: string) {
+    this.storage.set("token", `Token ${token}`);
+  }
+
+  _login(username: string, password: string): Observable<any> {
+    return this.http
+      .post(environment.loginUser, { username: username, password: password })
+      .pipe(retry(2), catchError(this.handleError));
+  }
+
+  login(username, password) {
+    this._login(username, password)
+      .pipe(takeWhile(() => this.alive))
+      .subscribe((res) => {
+        if (res) {
+          if (res.status == "200") {
+            this.setToken(res.token);
+            this.loginState.next(res);
+            this.authenticationState.next(true);
+          } else {
+            this.presentErrorAlert(
+              "Warning",
+              "Check login credentials and try again"
+            );
+          }
+        } else {
+          this.presentErrorAlert("Error", "Unable to login user");
+        }
+      });
+  }
+
+  async presentErrorAlert(type: string, msg: string) {
+    const alert = await this.alertController.create({
+      header: type,
+      message: msg,
+      buttons: ["OK"],
+    });
+
+    await alert.present();
+  }
+
+  isAuthenticated(): boolean {
+    return this.authenticationState.value;
+  }
+
+  logout() {
+    this.storage.remove("token").then(() => {
+      this.authenticationState.next(false);
+      this.router.navigateByUrl["/login"];
+    });
+  }
+
+  handleError(error: HttpErrorResponse) {
+    return throwError(error);
+  }
+
+  ngOnDestroy() {
+    this.alive = false;
+  }
+}
