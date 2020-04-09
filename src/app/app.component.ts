10bc3b9fb03ead2c395f9491dc672055c21aae3b
Damilola Oluwafemi <damilolaoluwafemi@Damilolas-MacBook-Pro.local>
2020-04-09 10:42:09 +0100

    replaced side menu content with live data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,23 +1,30 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 
 import { Platform } from "@ionic/angular";
 import { SplashScreen } from "@ionic-native/splash-screen/ngx";
 import { StatusBar } from "@ionic-native/status-bar/ngx";
 import { AuthenticationService } from "./Auth/authentication.service";
 import { Router } from "@angular/router";
+import { HomeService } from "./Home/home.service";
+import { Profile } from "./models/profile.model";
 
 @Component({
   selector: "app-root",
   templateUrl: "app.component.html",
   styleUrls: ["app.component.scss"],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  profileDetails: Profile;
+  profilePicture: string;
+  username: string;
+
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
     private statusBar: StatusBar,
     private authService: AuthenticationService,
-    private router: Router
+    private router: Router,
+    private homeService: HomeService
   ) {
     this.initializeApp();
   }
@@ -37,7 +44,22 @@ export class AppComponent {
     });
   }
 
+  ngOnInit() {
+    this.getProfile();
+  }
+
+  getProfile() {
+    this.homeService.profileDetails.subscribe((res) => {
+      if (res) {
+        console.log(res, "res");
+        this.profilePicture = res["profile_picture"];
+        this.username = res["name"];
+      }
+    });
+  }
+
   onLogout() {
     this.authService.logout();
+    this.router.navigateByUrl("/login");
   }
 }
